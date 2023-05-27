a1a5f6e92efb051f7dfb8849e8856d374de74249
Your Name <mayasinalu@gmail.com>
2023-05-27 21:19:13 +0300

    grade

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+// Creating a variable and giving it a value between 1 and 100
+const grade = 77;
+
+if (grade >= 90 && grade <= 100) {
+  console.log("You got an A grade");
+} else if (grade >= 75 && grade <= 90) {
+  console.log("You got a B grade");
+} else if (grade >= 60 && grade <= 75) {
+  console.log("You got a C grade");
+} else {
+  console.log("You FAILED!");
+}
